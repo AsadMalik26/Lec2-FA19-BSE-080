7e2a8869c52b91690ee69873b2116f9e6fe2f009
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2022-02-25 23:49:18 +0500

    Football Lab3-4 Done

---
+++ Assets/camera_script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_script : MonoBehaviour
+{
+    public Vector3 offset;
+    public GameObject football;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - football.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = football.transform.position + offset;
+        print(transform.position);
+    }
+}
